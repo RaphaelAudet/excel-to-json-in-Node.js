72c7ff7b7ff52429eb1f746f11d667b2be71d9e7
Raphael <raphael@packitoo.com>
2019-04-12 09:31:29 +0200

    updating xls lib and adding filesystem explorer code

--- app.js
+++ app.js
@@ -3,10 +3,51 @@
     var bodyParser = require('body-parser');
     var multer = require('multer');
     var xlstojson = require("xls-to-json-lc");
-    var xlsxtojson = require("xlsx-to-json-lc");
+    var xlsxtojson = require("xlsx-to-json-lc-hoffer");
     
 
-    app.use(bodyParser.json());  
+    app.use(bodyParser.json()); 
+
+    var readXls = function(file, res) {
+        try {
+            xlstojson({
+                input: file.path,
+                output: null, //since we don't need output.json
+                lowerCaseHeaders:true
+            }, function(err,result){
+                if(err) {
+                    return res.json({error_code:1,err_desc:err, data: null});
+                } 
+                res.json({error_code:0,err_desc:null, data: result});
+            });
+        } catch (e){
+            res.json({error_code:1,err_desc:"Corupted excel file"});
+        }
+    }
+
+    // List all files in a directory in Node.js recursively in a synchronous fashion
+    var walkSync = function(dir, filelist) {
+        var path = path || require('path');
+        var fs = fs || require('fs'),
+            files = fs.readdirSync(dir);
+        filelist = filelist || [];
+        files.forEach(function(file) {
+        if (fs.statSync(path.join(dir, file)).isDirectory()) {
+            filelist = walkSync(path.join(dir, file), filelist);
+        }
+        else {
+            if (file.endsWith(".xls")) {
+                let theFile = path.join(dir, file);
+                filelist.push(theFile);
+                if (file.endsWith("CARTONETTES145X75.xls")) {
+                    console.log(theFile);
+                    console.log(readXls(theFile, null));
+                }
+            }
+        }
+        });
+        return filelist;
+    };
 
     var storage = multer.diskStorage({ //multers disk storage settings
         destination: function (req, file, cb) {
@@ -74,4 +115,6 @@
 
     app.listen('3000', function(){
         console.log('running on 3000...');
+        // console.log(walkSync('..',null));
+        walkSync('..',null);
     });
